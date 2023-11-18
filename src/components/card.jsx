6651f47924cb36811ac6821d007dc04193972935
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-11-19 00:00:27 +0530

    booking added

---
+++ src/components/card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const RoundedCard = () => {
+  return (
+    <div className="text-white hover:text-black border-2 border-white hover:border-black relative overflow-hidden rounded-lg p-6 shadow-2xl transition duration-300 ease-in-out hover:opacity-100 transform hover:scale-105 hover:bg-white">
+      <h2 className="text-2xl font-bold ">Cloud Study Jam</h2>
+      <h3 className="text-2xl font-semibold mb-4">GDSC</h3>
+      <h4 className="text-2xl font-bold mb-2">APJ HALL</h4>
+      <div className="border-b-4 rounded-xl border-gray-300 mb-4"></div>
+      <div className='flex flex-row justify-between'>
+        <div className='font-bold'>20 Nov 2023</div>
+        <div className='font-bold'>5 pm</div>
+      </div>
+    </div>
+  );
+};
+
+export default RoundedCard;
--- src/pages/book.js
+++ src/pages/book.js
@@ -1,9 +1,24 @@
 import Navbar from "@/components/Navbar";
+import Card from '../components/card';
+// import eventlist from '../components/eventlist';
 
 export default function Booking() {
+//   const cards = eventlist.map(item => (
+//     <Eventcard items={item} />
+// ));
   return (
     <main>  
         <Navbar/>
+        <div className='mt-10 md:ml-20 md:mr-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-16 md:gap-8 lg:gap-16'>
+              <Card/>
+              <Card/>
+              <Card/>
+              <Card/>
+              {/* {cards} */}
+              <Card/>
+              <Card/>
+              <Card/>
+        </div>
     </main>
   )
 }
