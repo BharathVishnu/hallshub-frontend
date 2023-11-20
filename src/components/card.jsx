7cf4134ccf135d7aa3e9773a0299511f379a4007
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-11-21 01:30:05 +0530

    supabase integration

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../pages/supabase';
 
 const Card = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchDataFromSupabase = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('user')
+          .select('*');
+        console.log(data);
+        if (error) {
+          console.error('Error fetching data from Supabase:', error.message);
+        } else {
+          setUserData(data);
+        }
+      } catch (error) {
+        console.error('Unexpected error:', error.message);
+      }
+    };
+
+    fetchDataFromSupabase();
+  }, []); 
+
   return (
-    <div className=" font-mont card text-white hover:text-black border-2 border-white hover:border-black relative overflow-hidden rounded-lg p-6 shadow-2xl transition duration-300 ease-in-out hover:opacity-100 transform hover:scale-105 hover:bg-white">
-      <h2 className="text-2xl font-bold ">Cloud Study Jam</h2>
+    <div className="font-mont card text-white hover:text-black border-2 border-white hover:border-black relative overflow-hidden rounded-lg p-6 shadow-2xl transition duration-300 ease-in-out hover:opacity-100 transform hover:scale-105 hover:bg-white">
+      <h2 className="text-2xl font-bold">Cloud Study Jam</h2>
       <h3 className="text-2xl font-semibold mb-4">GDSC</h3>
       <h4 className="text-2xl font-bold mb-2">APJ HALL</h4>
       <div className="border-b-4 rounded-xl border-gray-300 mb-4"></div>
-      <div className='flex flex-row justify-between'>
-        <div className='font-bold'>20 Nov 2023</div>
-        <div className='font-bold'>5.00</div>
-      </div>
+      {userData ? (
+        <div className='flex flex-row justify-between'>
+          <div className='font-bold'>20 Nov 2023</div>
+          <div className='font-bold'>5.00</div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createClient } from '@supabase/supabase-js';
+const supabasekey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxcXpwa2F5aXZmZXJ6enpuc3JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxNTQ4NzAsImV4cCI6MjAxNTczMDg3MH0.dmhOCOt-RU9f1FnxFvcRDDwz6wY7xm5KdZggNc81mNY";
+
+const supabase = createClient('https://hqqzpkayivferzzznsrn.supabase.co', supabasekey);
+
+//const express = require('supabase');
+//const app = express();
+//app.get('/', ()=>{
+     //supabase.from('tablename').
+//})
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
 }
