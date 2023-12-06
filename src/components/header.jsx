44f8f92c5e9b6486c67db4ed92ecf4f25a52e555
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-12-06 19:19:13 +0530

    changed header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div data-aos="fade-down" className='font-mont text-4xl font-bold text-center md:text-right font-mont md:mr-10 mt-10'>
-        halls<div className='text-right w-24 bg-[#FFA500] text-black'>Hub</div>
+        hallsHub
     </div>
   )
 }
